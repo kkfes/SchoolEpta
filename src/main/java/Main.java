61b7439ef894aa5a4cbfcbae0f3082f65e7ed5f9
kkfes <musiks005@gmail.com>
2022-08-13 20:51:20 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import bot.BotMain;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            new TelegramBotsApi(DefaultBotSession.class).registerBot(new BotMain());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
