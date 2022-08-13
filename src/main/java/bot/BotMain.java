e848976af4e195855c217943cc1c02703d46daeb
blackgitler <tsobdanilio3@gmail.com>
2022-08-13 18:28:21 +0300

    asas

--- src/main/java/bot/BotMain.java
+++ src/main/java/bot/BotMain.java
@@ -5,8 +5,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class BotMain extends TelegramLongPollingBot {
 
-    public static final String BOT_NAME = "";
-    public static final String BOT_TOKEN = "";
+    public static final String BOT_NAME = "scholotron_bot";
+    public static final String BOT_TOKEN = "5478245744:AAGItaRwXXMfIpdYezpmLbosLIfZBXf2mUs";
 
 
     @Override
