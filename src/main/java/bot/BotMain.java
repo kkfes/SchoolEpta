ecb4004f70b98d5dc4475f2356f6586da39947e2
kkfes <musiks005@gmail.com>
2022-08-13 21:32:11 +0600

    first

--- src/main/java/bot/BotMain.java
+++ src/main/java/bot/BotMain.java
@@ -1,7 +1,9 @@
 package bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class BotMain extends TelegramLongPollingBot {
 
@@ -21,6 +23,24 @@ public class BotMain extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage()&&update.getMessage().hasText()){
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setParseMode("HTML");
+            sendMessage.setText(messageListener(update.getMessage().getText()));
+            sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+
+    public static String messageListener(String message){
+        if(message.equals("/start")){
+            return "\uD83D\uDC4B";
+        }
+        return "\uD83E\uDD37\u200D♂️";
     }
 }
